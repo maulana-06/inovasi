2024ea3533f25cd1148d97db62d6887e5a0cf119
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-11-09 10:44:40 +0700

    Fix syntax error to PostgreSQL-08

--- api/database.js
+++ api/database.js
@@ -1,11 +1,9 @@
 // File: database.js 
 const { Pool } = require('pg');
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, 
   ssl: {
     rejectUnauthorized: false   }
-});
+}); 
 
-// Ekspor pool agar bisa digunakan di route API Anda
 module.exports = pool;
