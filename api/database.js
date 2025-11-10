bc7ab6109a61eb873572db05c7077f5f66e10ed5
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-11-10 15:09:04 +0700

    Fix syntax error to PostgreSQL-13

--- api/database.js
+++ api/database.js
@@ -2,9 +2,21 @@
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
-  ssl: {
-    rejectUnauthorized: false   }
-}); 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  host: process.env.DB_HOST, 
+  database: process.env.DB_DATABASE, 
+  port: 5432,
+    ssl: {
+    rejectUnauthorized: false,
+    // Baris ini terkadang membantu memaksa koneksi
+    requestCert: true 
+  },
+  
+  // Aturan Pool Connection
+  max: 20, // Batasi jumlah koneksi di Vercel
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 module.exports = pool;
