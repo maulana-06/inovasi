0409b823413f9271549a2ca9d4bc1dcd0c6e5070
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-11-12 17:06:42 +0700

    tenant-aware to Vercel PostgreSQL-17

--- api/database.js
+++ api/database.js
@@ -7,10 +7,10 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_DATABASE, 
   port: process.env.DB_PORT,
-//    ssl: {
-//    rejectUnauthorized: false,
-//    requestCert: true 
-//  },
+    ssl: {
+    rejectUnauthorized: false,
+    requestCert: true 
+  },
     connectionTimeoutMillis: 10000, // Naikkan menjadi 10 detik
     idleTimeoutMillis: 30000, // koneksi idle selama 30 detik
     max: 20, // Jumlah koneksi maksimum
